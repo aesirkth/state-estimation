a89f1a4c546bd29c17199570532d47759b815869
AnderssonL <fabian.andersson2005@gmail.com>
2025-03-22 22:08:24 +0100

    noise and bias finder program

---
+++ PDF-analysis/sensor-noise-bias-finder.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+
+
+"""
+The following code is meant to collect a large sample of sensor readings when the sensor is static and with a true value X.
+The code will take N readings from the sensor and create a PDF of it's gaussian output x and give the covariance (noise) and
+potential bias. This information can be used when tuning filtering algorithms.
+"""
+
+def get_sensor():
+    # Some code that retrieves the sensor reading from a selected sensor.
+    # For now lets have a gaussian distribution function here
+    return np.random.normal(loc=0, scale=1, size=1).item()
+
+N = 1000 # number of samples
+X = 0 # expected value (for static accelerometer = 0)
+samples = [] # sample list
+
+# retrieve samples
+for i in range(N):
+    samples.append(get_sensor())
+
+# Fit a gaussian PDF onto the samples using kernel density estimation
+sample_pdf = gaussian_kde(samples)
+covariance = sample_pdf.covariance
+mean = np.mean(samples)
+
+# print information
+print('Noise/Covariance: ', covariance)
+print('Bias: ', X-mean)
+
+# Plot the estimated PDF
+xs = np.linspace(min(samples), max(samples), N)
+plt.plot(xs, sample_pdf(xs), label="Estimated PDF (KDE)")
+plt.xlabel("x")
+plt.ylabel("Probability Density")
+plt.title("PDF of sensor samples")
+plt.axvline(x=X, color='r', linestyle='--', label='actual value')
+plt.axvspan(X, mean, color='red', alpha=0.5, label='bias')
+plt.legend()
+plt.show()
+
+
+
