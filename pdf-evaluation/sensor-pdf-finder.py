65eefbe38155a279651498b7f179560beab85751
AnderssonL <fabian.andersson2005@gmail.com>
2025-03-28 20:05:43 +0100

    rpi update

--- pdf-evaluation/sensor-pdf-finder.py
+++ pdf-evaluation/sensor-pdf-finder.py
@@ -1,26 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
+from mpu6050 import mpu6050
+import Adafruit_BMP.BMP085 as BMP085
+import time
 
 
 """
 The following code is meant to collect a large sample of sensor readings when the sensor is static and with a true value X.
 The code will take N readings from the sensor and create a PDF of it's gaussian output x and give the covariance (noise) and
 potential bias. This information can be used when tuning filtering algorithms such as the Kalman Filter.
+
+The code here has been used on a raspberry pi.
 """
 
-def get_sensor():
+def get_sensor(mpu_sensor, baro_sensor):
     # Some code that retrieves the sensor reading from a selected sensor.
     # For now lets have a gaussian distribution function here
-    return np.random.normal(loc=3, scale=1, size=1).item()
+    accel_data = mpu_sensor.get_accel_data()
+    gyro_data = mpu_sensor.get_gyro_data()
+    
+    ax = accel_data['x']
+    ay = accel_data['y']
+    az = accel_data['z']
 
-N = 10000 # number of samples
+    gx = gyro_data['x']
+    gy = gyro_data['y']
+    gz = gyro_data['z']
+
+    pressure = baro_sensor.read_pressure()
+
+    return ax
+    
+
+N = 1000 # number of samples
 X = 0 # expected value (for static accelerometer = 0)
 samples = [] # sample list
 
+# Initialize the MPU6050 and BMP180 sensor
+mpu_sensor = mpu6050(0x68)
+baro_sensor = BMP085.BMP085(busnum=1)
+
+counter = 0
 # retrieve samples
 for i in range(N):
-    samples.append(get_sensor())
+    samples.append(get_sensor(mpu_sensor, baro_sensor))
+    time.sleep(0.01)
+    counter+=1
+    print(counter)
 
 # Fit a gaussian PDF onto the samples using kernel density estimation
 sample_pdf = gaussian_kde(samples)
